a289c104edcc8e1deef7b0ae51759919aedb3ffb
Lionel <lionelteojiarong@gmail.com>
2021-01-22 17:49:47 +0800

    Merge files
    added login, postjob and user profile

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import IndividualJobPage from './components/individualjobpage/IndividualJobPage';
 import PostReviewPage from './components/PostReviewPage';
+import Login from './components/Login';
+import UserProfile from './components/UserProfile';
 function App() {
   return (
     <Fragment>
        <Router>
                 <Switch>
+                    <Route exact path="/" component={Login}/>
+                    <Route exact path="/profile" component={UserProfile}/>
                     <Route exact path="/home" component={Home} />
                     <Route exact path="/jobs/:id" component={IndividualJobPage} />
                     <Route exact path="/review/:id" component={PostReviewPage} />
